b57f100671739eb75361ed947768f59937a3b4fe
Gopal Krishna Jha <141767531+gopal-krishna-jha@users.noreply.github.com>
2025-03-06 10:24:11 +0530

    [Gopal Krishna Jha] Add Angular Welcome File.
    [Gopal Krishna Jha] Add Angular Welcome File.

---
+++ WelcomeToWeek8Angular.java
@@ -0,0 +1,6 @@
+//Create a WelcomeToWeek08Angular to print the welcome statement
+public class WelcomeToWeek08Angular{
+    public static void main(String[] args) {
+        System.out.println("\"Welcome to Week-08 Angular Getting Started!\"");
+    }
+}
